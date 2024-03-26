b38f75b334c745b02a2c59ff636ef4c6bf0ebb2b
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-26 15:48:58 -0300

    Renomeado: SantanderDevweek2024Application para Application

--- src/main/java/me/dio/sdw24/Application.java
+++ src/main/java/me/dio/sdw24/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevweek2024Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevweek2024Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/me/dio/sdw24/ApplicationTests.java
+++ src/test/java/me/dio/sdw24/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantanderDevweek2024ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
