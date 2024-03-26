a6860805cee5345303db19a13dddfba794c6b93b
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-03-26 15:37:40 -0300

    Initial commit

---
+++ src/main/java/me/dio/sdw24/SantanderDevweek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.sdw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevweek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevweek2024Application.class, args);
+	}
+
+}
